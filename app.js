5f6c96ffbf32910d00f91c104a1ec999265e34dc
amaraa <oamraa62@gmail.com>
2021-05-28 23:07:05 +0800

    tosliig yronhiid ni tuusgav

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var uiController = (function() {
+// Ð”ÑÐ»Ð³ÑÑ†Ñ‚ÑÐ¹ Ð°Ð¶Ð¸Ð»Ð»Ð°Ñ… ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
+var uiController = (function () {
   var DOMstrings = {
     inputType: ".add__type",
     inputDescription: ".add__description",
@@ -10,23 +11,69 @@ var uiController = (function() {
     incomeLabel: ".budget__income--value",
     expeseLabel: ".budget__expenses--value",
     percentageLabel: ".budget__expenses--percentage",
-    containerDiv: ".container"
+    containerDiv: ".container",
+    expensePercentageLabel: ".item__percentage",
+    dateLabel: ".budget__title--month",
+  };
+
+  var nodeListForeach = function (list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
+  var formatMoney = function (number, type) {
+    number = "" + number;
+    var splitter = number.split("").reverse().join("");
+    var count = 1;
+    var y = "";
+
+    for (var i = 0; i < splitter.length; i++) {
+      y += splitter[i];
+      if (count % 3 == 0) y = y + ",";
+      count++;
+    }
+
+    var z = y.split("").reverse().join("");
+    if (z[0] === ",") z = z.substr(1, y.length - 1);
+
+    type === "inc" ? (z = "+ " + z) : (z = "- " + z);
+
+    return z;
   };
 
   return {
-    getInput: function() {
+    displayDate: function () {
+      var today = new Date();
+
+      document.querySelector(DOMstrings.dateLabel).textContent =
+        today.getFullYear() + "оны " + today.getMonth() + "-р сарын ";
+    },
+    getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // exp, inc
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: parseInt(document.querySelector(DOMstrings.inputValue).value)
+        value: parseInt(document.querySelector(DOMstrings.inputValue).value),
       };
     },
 
-    getDOMstrings: function() {
+    displayPercentages: function (allPercentages) {
+      // Ð—Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ NodeList-Ð¸Ð¹Ð³ Ð¾Ð»Ð¾Ñ…
+      var elements = document.querySelectorAll(
+        DOMstrings.expensePercentageLabel
+      );
+
+      // Ð­Ð»ÐµÐ¼ÐµÐ½Ñ‚ Ð±Ð¾Ð»Ð³Ð¾Ð½Ñ‹ Ñ…ÑƒÐ²ÑŒÐ´ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ð¼Ð°ÑÑÐ¸Ð²Ð°Ð°Ñ Ð°Ð²Ñ‡ ÑˆÐ¸Ð²Ð¶ Ð¾Ñ€ÑƒÑƒÐ»Ð°Ñ…
+      nodeListForeach(elements, function (el, index) {
+        el.textContent = allPercentages[index];
+      });
+    },
+
+    getDOMstrings: function () {
       return DOMstrings;
     },
 
-    clearFields: function() {
+    clearFields: function () {
       var fields = document.querySelectorAll(
         DOMstrings.inputDescription + ", " + DOMstrings.inputValue
       );
@@ -34,32 +81,45 @@ var uiController = (function() {
       // Convert List to Array
       var fieldsArr = Array.prototype.slice.call(fields);
 
-      fieldsArr.forEach(function(el, index, array) {
+      fieldsArr.forEach(function (el, index, array) {
         el.value = "";
       });
 
       fieldsArr[0].focus();
     },
 
-    tusviigUzuuleh: function(tusuv) {
-      document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
-      document.querySelector(DOMstrings.incomeLabel).textContent = tusuv.totalInc;
-      document.querySelector(DOMstrings.expeseLabel).textContent = tusuv.totalExp;
+    tusviigUzuuleh: function (tusuv) {
+      var type;
+      tusuv.tusuv > 0 ? (type = "inc") : (type = "exp");
+      document.querySelector(DOMstrings.tusuvLabel).textContent = formatMoney(
+        tusuv.tusuv,
+        type
+      );
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatMoney(
+        tusuv.totalInc,
+        "inc"
+      );
+      document.querySelector(DOMstrings.expeseLabel).textContent = formatMoney(
+        tusuv.totalExp,
+        "exp"
+      );
 
       if (tusuv.huvi !== 0) {
-        if(tusuv.tusuv > 0) document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi + "%";
-        else document.querySelector(DOMstrings.percentageLabel).textContent = "0%";
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi + "%";
       } else {
-          document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi;
-      } 
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi;
+      }
     },
 
-    deleteListItem: function(id) {
+    deleteListItem: function (id) {
       var el = document.getElementById(id);
       el.parentNode.removeChild(el);
     },
 
-    addListItem: function(item, type) {
+    addListItem: function (item, type) {
+      // ÐžÑ€Ð»Ð¾Ð³Ð¾ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ ÑÐ»ÐµÐ¼ÐµÐ½Ñ‚Ð¸Ð¹Ð³ Ð°Ð³ÑƒÑƒÐ»ÑÐ°Ð½ html-Ð¸Ð¹Ð³ Ð±ÑÐ»Ñ‚Ð³ÑÐ½Ñ.
       var html, list;
       if (type === "inc") {
         list = DOMstrings.incomeList;
@@ -70,45 +130,47 @@ var uiController = (function() {
         html =
           '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
+      // Ð¢ÑÑ€ HTML Ð´Ð¾Ñ‚Ñ€Ð¾Ð¾ Ð¾Ñ€Ð»Ð¾Ð³Ð¾ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ ÑƒÑ‚Ð³ÑƒÑƒÐ´Ñ‹Ð³ REPLACE Ð°ÑˆÐ¸Ð³Ð»Ð°Ð¶ Ó©Ó©Ñ€Ñ‡Ð¸Ð»Ð¶
       html = html.replace("%id%", item.id);
       html = html.replace("$$DESCRIPTION$$", item.description);
-      html = html.replace("$$VALUE$$", item.value);
+      html = html.replace("$$VALUE$$", formatMoney(item.value, type));
 
+      // Ð‘ÑÐ»Ñ‚Ð³ÑÑÑÐ½ HTML ÑÑ DOM Ñ€ÑƒÑƒ Ñ…Ð¸Ð¹Ð¶ Ó©Ð³Ð½Ó©.
       document.querySelector(list).insertAdjacentHTML("beforeend", html);
-    }
+    },
   };
 })();
 
-var financeController = (function() {
+// Ð¡Ð°Ð½Ñ…Ò¯Ò¯Ñ‚ÑÐ¹ Ð°Ð¶Ð¸Ð»Ð»Ð°Ñ… ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
+var financeController = (function () {
   // private data
-  var Income = function(id, description, value) {
+  var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
   // private data
-  var Expense = function(id, description, value) {
+  var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
     this.percentage = -1;
   };
 
-  Expense.prototype.calcPercentage = function(totalIncome){
-    if(totalIncome > 0)
+  Expense.prototype.calcPercentage = function (totalIncome) {
+    if (totalIncome > 0)
       this.percentage = Math.round((this.value / totalIncome) * 100);
-    else
-      this.percentage = 0;
+    else this.percentage = 0;
   };
 
-  Expense.prototype.getPercentage = function() {
+  Expense.prototype.getPercentage = function () {
     return this.percentage;
-  }
+  };
 
-  var calculateTotal = function(type) {
+  var calculateTotal = function (type) {
     var sum = 0;
-    data.items[type].forEach(function(el) {
+    data.items[type].forEach(function (el) {
       sum = sum + el.value;
     });
 
@@ -119,56 +181,61 @@ var financeController = (function() {
   var data = {
     items: {
       inc: [],
-      exp: []
+      exp: [],
     },
 
     totals: {
       inc: 0,
-      exp: 0
+      exp: 0,
     },
 
     tusuv: 0,
 
-    huvi: 0
+    huvi: 0,
   };
 
   return {
-    tusuvTootsooloh: function() {
+    tusuvTootsooloh: function () {
+      // ÐÐ¸Ð¹Ñ‚ Ð¾Ñ€Ð»Ð¾Ð³Ñ‹Ð½ Ð½Ð¸Ð¹Ð»Ð±ÑÑ€Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
       calculateTotal("inc");
 
+      // ÐÐ¸Ð¹Ñ‚ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ Ð½Ð¸Ð¹Ð»Ð±ÑÑ€Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
       calculateTotal("exp");
 
+      // Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ ÑˆÐ¸Ð½ÑÑÑ€ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
       data.tusuv = data.totals.inc - data.totals.exp;
-      if(data.totals.inc > 0)
-          data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
-      else 
-          data.huvi = 0; 
+
+      // ÐžÑ€Ð»Ð¾Ð³Ð¾ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
+      if (data.totals.inc > 0)
+        data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
+      else data.huvi = 0;
     },
 
-    calculatePercentages: function(){
-        var allPercentages = data.items.exp.forEach(function(el){
-          el.calcPercentage(data.totals.inc);
-        });
-        return allPercentages;
+    calculatePercentages: function () {
+      data.items.exp.forEach(function (el) {
+        el.calcPercentage(data.totals.inc);
+      });
     },
 
-    getPercentages: function(){
-        data.items.exp.map(function(el){
-          return el.getPercentage();
-        })
+    getPercentages: function () {
+      var allPercentages = data.items.exp.map(function (el) {
+        return el.getPercentage();
+      });
+
+      return allPercentages;
     },
 
-    tusviigAvah: function() {
+    tusviigAvah: function () {
       return {
         tusuv: data.tusuv,
         huvi: data.huvi,
         totalInc: data.totals.inc,
-        totalExp: data.totals.exp
+        totalExp: data.totals.exp,
       };
     },
 
-    deleteItem: function(type, id) {
-      var ids = data.items[type].map(function(el) {
+    deleteItem: function (type, id) {
+      var ids = data.items[type].map(function (el) {
         return el.id;
       });
 
@@ -179,7 +246,7 @@ var financeController = (function() {
       }
     },
 
-    addItem: function(type, desc, val) {
+    addItem: function (type, desc, val) {
       var item, id;
 
       if (data.items[type].length === 0) id = 1;
@@ -198,52 +265,63 @@ var financeController = (function() {
       return item;
     },
 
-    seeData: function() {
+    seeData: function () {
       return data;
-    }
+    },
   };
 })();
 
-var appController = (function(uiController, financeController) {
-  var ctrlAddItem = function() {
+// ÐŸÑ€Ð¾Ð³Ñ€Ð°Ð¼Ñ‹Ð½ Ñ…Ð¾Ð»Ð±Ð¾Ð³Ñ‡ ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. ÐžÑ€ÑƒÑƒÐ»Ð°Ñ… Ó©Ð³Ó©Ð³Ð´Ð»Ð¸Ð¹Ð³ Ð´ÑÐ»Ð³ÑÑ†ÑÑÑ Ð¾Ð»Ð¶ Ð°Ð²Ð½Ð°.
     var input = uiController.getInput();
 
     if (input.description !== "" && input.value !== "") {
+      // 2. ÐžÐ»Ð¶ Ð°Ð²ÑÐ°Ð½ Ó©Ð³Ó©Ð³Ð´Ð»Ò¯Ò¯Ð´ÑÑ ÑÐ°Ð½Ñ…Ò¯Ò¯Ð³Ð¸Ð¹Ð½ ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€Ñ‚ Ð´Ð°Ð¼Ð¶ÑƒÑƒÐ»Ð¶ Ñ‚ÑÐ½Ð´ Ñ…Ð°Ð´Ð³Ð°Ð»Ð½Ð°.
       var item = financeController.addItem(
         input.type,
         input.description,
         input.value
       );
 
+      // 3. ÐžÐ»Ð¶ Ð°Ð²ÑÐ°Ð½ Ó©Ð³Ó©Ð³Ð´Ð»Ò¯Ò¯Ð´ÑÑ Ð²ÑÐ± Ð´ÑÑÑ€ÑÑ Ñ‚Ð¾Ñ…Ð¸Ñ€Ð¾Ñ… Ñ…ÑÑÑÐ³Ñ‚ Ð½ÑŒ Ð³Ð°Ñ€Ð³Ð°Ð½Ð°
       uiController.addListItem(item, input.type);
       uiController.clearFields();
 
+      // Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ ÑˆÐ¸Ð½ÑÑÑ€ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð¾Ð¾Ð´ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ò¯Ð·Ò¯Ò¯Ð»Ð½Ñ.
       updateTusuv();
     }
   };
 
-  var updateTusuv = function() {
+  var updateTusuv = function () {
+    // 4. Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
     financeController.tusuvTootsooloh();
 
+    // 5. Ð­Ñ†ÑÐ¸Ð¹Ð½ Ò¯Ð»Ð´ÑÐ³Ð´ÑÐ»,
     var tusuv = financeController.tusviigAvah();
 
+    // 6. Ð¢Ó©ÑÐ²Ð¸Ð¹Ð½ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð³ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ð³Ð°Ñ€Ð³Ð°Ð½Ð°.
     uiController.tusviigUzuuleh(tusuv);
 
+    // 7. Ð­Ð»ÐµÐ¼ÐµÐ½Ñ‚Ò¯Ò¯Ð´Ð¸Ð¹Ð½ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
     financeController.calculatePercentages();
 
+    // 8. Ð­Ð»ÐµÐ¼ÐµÐ½Ñ‚Ò¯Ò¯Ð´Ð¸Ð¹Ð½ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ñ…Ò¯Ð»ÑÑÐ¶ Ð°Ð²Ð½Ð°
     var allPercentages = financeController.getPercentages();
 
-    
+    // 9. Ð­Ð´Ð³ÑÑÑ€ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ð³Ð°Ñ€Ð³Ð°Ð½Ð°.
+    uiController.displayPercentages(allPercentages);
   };
 
-  var setupEventListeners = function() {
+  var setupEventListeners = function () {
     var DOM = uiController.getDOMstrings();
 
-    document.querySelector(DOM.addBtn).addEventListener("click", function() {
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
     });
 
-    document.addEventListener("keypress", function(event) {
+    document.addEventListener("keypress", function (event) {
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
       }
@@ -251,7 +329,7 @@ var appController = (function(uiController, financeController) {
 
     document
       .querySelector(DOM.containerDiv)
-      .addEventListener("click", function(event) {
+      .addEventListener("click", function (event) {
         var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
 
         if (id) {
@@ -261,25 +339,32 @@ var appController = (function(uiController, financeController) {
           var itemId = parseInt(arr[1]);
 
           console.log(type + " ===> " + itemId);
+
+          // 1. Ð¡Ð°Ð½Ñ…Ò¯Ò¯Ð³Ð¸Ð¹Ð½ Ð¼Ð¾Ð´ÑƒÐ»Ð¸Ð°Ñ type, id Ð°ÑˆÐ¸Ð³Ð»Ð°Ð°Ð´ ÑƒÑÑ‚Ð³Ð°Ð½Ð°.
           financeController.deleteItem(type, itemId);
+
+          // 2. Ð”ÑÐ»Ð³ÑÑ† Ð´ÑÑÑ€ÑÑÑ ÑÐ½Ñ ÑÐ»ÐµÐ¼ÐµÐ½Ñ‚Ð¸Ð¹Ð³ ÑƒÑÑ‚Ð³Ð°Ð½Ð°
           uiController.deleteListItem(id);
 
+          // 3. Ò®Ð»Ð´ÑÐ³Ð´ÑÐ» Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð³ ÑˆÐ¸Ð½ÑÑ‡Ð¸Ð»Ð¶ Ñ…Ð°Ñ€ÑƒÑƒÐ»Ð½Ð°.
+          // Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ ÑˆÐ¸Ð½ÑÑÑ€ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð¾Ð¾Ð´ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ò¯Ð·Ò¯Ò¯Ð»Ð½Ñ.
           updateTusuv();
         }
       });
   };
 
   return {
-    init: function() {
+    init: function () {
       console.log("Application started...");
+      uiController.displayDate();
       uiController.tusviigUzuuleh({
         tusuv: 0,
         huvi: 0,
         totalInc: 0,
-        totalExp: 0
+        totalExp: 0,
       });
       setupEventListeners();
-    }
+    },
   };
 })(uiController, financeController);
 
